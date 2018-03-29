d47c669d1cf5743a6ef54e38a30ada5364afb3f3
Charu29 <charusharma2912@gmail.com>
2018-03-29 23:49:18 +0530

    Optimized

--- Projects.py
+++ Projects.py
@@ -1,7 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import re
-from bs4 import NavigableString
 
 
 def crawler():
@@ -9,25 +7,20 @@ def crawler():
         source_code = requests.get(url)
         text = source_code.text
         soup = BeautifulSoup(text, "html.parser")
-        
-        nos = []
-        for div in soup.find_all(string=re.compile("Total Submissions")):
-                txt = str(div)
-                no = ""
-                for i, c in enumerate(txt):
-                        if i == 41 or i == 42 or i == 43:
-                                no = no + c
-                nos.append(int(no))
-
-        href = []
-        titles = []
-        for div in soup.find_all("div", class_="title"):
-                href.append(div.a.get('href'))
-                titles.append(div.a.get_text())
-
-        for i in range(len(titles)):
-                if nos[i] < 10:
-                     print(nos[i], ":" + titles[i] + ":" + href[i])
+        for i in soup.findAll('div', {'class': 'head-part'}):
+                tss=i.find('div', {'class':'desc-info'})
+                h=i.div.string
+                print(h)
+                a=tss.get_text()
+                a=str(a)
+                print(a)
+                b=a[172:175]
+                print(b)
+                b=int(b)
+                print(b)
+                '''  if(b<=100):
+                   link = i.a.get('href')
+                   title = i.a.string
+                   print(title, link) '''
                 
-
 crawler()
